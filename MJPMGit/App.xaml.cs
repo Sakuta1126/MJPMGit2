9e689fa231205b7570bf1fcc63f1d282b14b48f2
Sakuta1126 <maildosteam@gmail.com>
2024-01-22 09:40:11 +0100

    Dodano baze danych

--- MJPMGit/App.xaml.cs
+++ MJPMGit/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Configuration;
+﻿using MJPMGit.Widoki.Klasy;
+using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace MJPMGit
@@ -9,6 +11,34 @@ namespace MJPMGit
     /// </summary>
     public partial class App : Application
     {
+        private static Baza Baza;
+        public static Baza baza
+        {
+            get
+            {
+                
+                if (Baza == null)
+                {
+                    Baza = new Baza(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "wideo.db"));
+                }
+                
+                return Baza;
+            }
+        }
+        public static bool SprawdzDlugosc(string teskt, int minimum, int maximum)
+        {
+            if (teskt.Length >= minimum && teskt.Length <= maximum)
+            { return true; }
+            else { return false; }
+            
+            
+            
+           
+           
+
+
+        }
+
     }
 
 }
--- MJPMGit/Widoki/Klasy/Baza.cs
+++ MJPMGit/Widoki/Klasy/Baza.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MJPMGit.Widoki.Klasy
 {
-    internal class Baza
+    public class Baza
     {
+        private readonly SQLiteConnection baza;
+        public Baza(string path)
+        {
+            baza = new SQLiteConnection(path);
+            baza.CreateTable<Wideo>();
+        }
+            public int Zapisz<T>(T obiekt)
+            {
+            return baza.Insert(obiekt);
+            } 
+            public int Usun<T>(T obiekt)
+            {
+            return baza.Delete(obiekt);
+            } 
+            public List<T> Pobierz<T>() where T : new()
+            {
+            return baza.Table<T>().ToList();
+            }
     }
 }
