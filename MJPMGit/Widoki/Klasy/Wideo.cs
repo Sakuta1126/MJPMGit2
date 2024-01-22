0d0138e95dfba3f7433fa5ec97c3068e86687249
p-majchrzak <pjoterm606@gmail.com>
2024-01-22 09:32:29 +0100

    Uzupełniono kod dla klasy Wideo.

--- MJPMGit/Widoki/Klasy/Wideo.cs
+++ MJPMGit/Widoki/Klasy/Wideo.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MJPMGit.Widoki.Klasy
 {
-    internal class Wideo
+    public class Wideo
     {
+        [AutoIncrement,PrimaryKey]
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+        public string Link { get; set; }
+        public Wideo() { }
+
+        public Wideo(string nazwa, string opis, string link)
+        {
+            Nazwa = nazwa;
+            Opis = opis;
+            Link = link;
+        }
     }
 }
