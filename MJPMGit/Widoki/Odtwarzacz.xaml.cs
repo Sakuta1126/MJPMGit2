30e12c9248e9efd54991c37e8c59b8b88752f6ba
Sakuta1126 <maildosteam@gmail.com>
2024-01-22 10:01:05 +0100

    dodano widok i logike odtwarzacza

--- MJPMGit/Widoki/Odtwarzacz.xaml.cs
+++ MJPMGit/Widoki/Odtwarzacz.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MJPMGit.Widoki.Klasy;
+using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using System.IO;
+using Path = System.IO.Path;
 namespace MJPMGit.Widoki
 {
     /// <summary>
@@ -19,9 +22,45 @@ namespace MJPMGit.Widoki
     /// </summary>
     public partial class Odtwarzacz : Window
     {
-        public Odtwarzacz()
+        MMDevice urzadzenie;
+        MMDeviceEnumerator wyznacznik;
+        Wideo globalnewideo;
+        public Odtwarzacz(Wideo wideo)
         {
             InitializeComponent();
+            wyznacznik = new MMDeviceEnumerator();
+            urzadzenie = wyznacznik.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            globalnewideo = wideo;
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            element_wideo.Source = new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Pliki") + "/" + globalnewideo.Link, UriKind.RelativeOrAbsolute);
+            element_wideo.LoadedBehavior = MediaState.Manual;
+            element_wideo.UnloadedBehavior = MediaState.Manual;
+            element_wideo.Play();
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            element_wideo.Play();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            element_wideo.Pause();
+        }
+
+        private void od_nowa_Click(object sender, RoutedEventArgs e)
+        {
+            element_wideo.Source=new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Pliki") + "/" + globalnewideo.Link,UriKind.RelativeOrAbsolute   );
+            element_wideo.Play();
+        }
+
+        private void dzwiek_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            urzadzenie.AudioEndpointVolume.MasterVolumeLevelScalar = (float)dzwiek.Value / 100;
+
         }
     }
 }
